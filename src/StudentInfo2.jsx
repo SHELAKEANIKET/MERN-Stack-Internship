32a4d07cdfdd2675d46e9282ae0bfaad6ba7eac4
Aniket Shelake <97660224+SHELAKEANIKET@users.noreply.github.com>
2024-01-18 17:22:26 +0530

    React Code Files

---
+++ src/StudentInfo2.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class StudentInfo2 extends Component {
+
+    constructor(){
+        super();
+        
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.position}</h1>
+      </div>
+    )
+  }
+}
